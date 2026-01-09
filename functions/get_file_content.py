e82cbdd831cb52d9314839da98234a0d73035ac5
Chirag Goyal <chiraggoyal4639@gmail.com>
2026-01-09 20:30:14 +0530

    feat: added functionality to run python file

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,5 @@
 import os
-from config import MAX_CHARS
+from constants import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
     try:
---
+++ functions/run_python_file.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=None):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_file = os.path.normpath(
+            os.path.join(working_dir_abs, file_path)
+        )
+        valid_target_file = (
+            os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+        )
+        if not valid_target_file:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(target_file):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+
+        if not file_path.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file'
+
+        command = ["python", target_file]
+        if args:
+            command.extend(args)
+        
+        # 'check=True' Validates target within working directory, and file existence before execution
+        # 'text=True' Decode the output to strings, rather than bytes; this is done by setting text=True
+        # 'capture_output=True' Capture the output of the command, so that it can be returned
+        # 'timeout=30' Set a timeout of 30 seconds, to prevent the command from running indefinitely
+        result = subprocess.run(command, text=True, capture_output=True, timeout=30, cwd=working_dir_abs)
+
+        output_str = ""
+        if result.returncode != 0:
+            output_str += f'Process exited with code {result.returncode}'
+        elif not (result.stdout or result.stderr):
+            output_str += "No output produced"
+        else:
+            if result.stdout:
+                output_str += f"STDOUT: {result.stdout}"
+            if result.stderr:
+                output_str += f"STDERR: {result.stderr}"
+
+        return output_str
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
