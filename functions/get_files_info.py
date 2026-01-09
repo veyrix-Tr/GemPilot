2e6803143c8b4e2db9517dbb603c2bf07cea0903
Chirag Goyal <chiraggoyal4639@gmail.com>
2026-01-09 17:34:10 +0530

    feat: added verbose flag and multi-msg system and added get_file_info and some tests for it

---
+++ functions/get_files_info.py
@@ -0,0 +1,32 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        
+        working_dir_abs = os.path.abspath(working_directory)
+
+        target_dir = os.path.normpath(
+            os.path.join(working_dir_abs, directory)
+        )
+
+        # Validate that the target directory is within the working directory and Now our LLM agent can't perform any work outside the working_directory that we give it
+        valid_target_dir = (
+            os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        )
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+
+        files = os.listdir(target_dir)
+        file_info=""
+        for file in files:
+            file_size = os.path.getsize(os.path.join(target_dir, file))
+            is_dir = os.path.isdir(os.path.join(target_dir, file))
+            file_info += (f"- {file}: file_size={file_size} bytes, is_dir={is_dir}\n")
+        return file_info
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import os
 import argparse
 from dotenv import load_dotenv
-from google import genai
+from google.genai import Client, types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+client = Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", default=False, help="Enable verbose mode")
 args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=args.user_prompt
+    contents=messages
 )
 
 def main():
     # print(response)
-    if response.usage_metadata:
-        print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
-        print("Response tokens: ", response.usage_metadata.candidates_token_count)
+    if args.verbose:
+        print("User prompt: ", args.user_prompt)
+        if response.usage_metadata:
+            print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+            print("Response tokens: ", response.usage_metadata.candidates_token_count)
     print("Response: \n",response.text)
 
 
